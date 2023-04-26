124641be86c769f3869ae60ba6c1c9ac8b86e23a
coder-gamerz <98739886+coder-gamerz@users.noreply.github.com>
2023-04-26 13:21:31 +0530

    added comments

--- Automation/PDF_Merger/PDF_merger.py
+++ Automation/PDF_Merger/PDF_merger.py
@@ -7,3 +7,6 @@ for file in os.listdir(r'C:\Users\shrey\OneDrive\Documents\Automation\PDF_Merger
     if file.endswith(".pdf"):
         merger.append(fr'C:\Users\shrey\OneDrive\Documents\Automation\PDF_Merger\PDFs\{file}')
     merger.write(r"C:\Users\shrey\OneDrive\Documents\Automation\PDF_Merger\Merged_PDFs\combined.pdf")
+    
+# A simple PDF merger
+    
