c10aa26a789ea8519a4373d314189cd7e2ed07b9
coder-gamerz <shrey1559@gmail.com>
2023-05-01 17:39:34 +0530

    added files for meme viewer in flask

--- Meme_Website_Flask/main.py
+++ Meme_Website_Flask/main.py
@@ -8,12 +8,11 @@ def get_meme():
     url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
-    subreddit = response["subreddit"]
-    return meme_large, subreddit
+    return meme_large
 
 @app.route('/')
 def index():
-    meme_pic,subreddit = get_meme()
-    return render_template("index.html", meme_pic=meme_pic, subreddit=subreddit)
+    meme_pic = get_meme()
+    return render_template("index.html", meme_pic=meme_pic)
 
 app.run(host='0.0.0.0', port=5000, debug=True)
