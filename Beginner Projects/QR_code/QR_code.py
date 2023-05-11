6db029410b4bf865f0de9f68ca8f8e304f43486b
coder-gamerz <shrey1559@gmail.com>
2023-05-11 17:01:47 +0530

    edited files for qr code generator

--- Beginner Projects/QR_code/QR_code.py
+++ Beginner Projects/QR_code/QR_code.py
@@ -1,7 +1,7 @@
 import pyqrcode as p
 from pyqrcode import QRCode
 
-link = 'https://stackoverflow.com'
+link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'
 qr = p.create(link)
 
 qr.png(r'C:\Users\shrey\OneDrive\Documents\My-Projects\Beginner Projects\QR_code\qr_codes\stack.png', scale=6)
