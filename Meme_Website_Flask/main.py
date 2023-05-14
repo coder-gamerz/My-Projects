04a22243d1757587186ce09dfb2a9a50e216255f
coder-gamerz <98739886+coder-gamerz@users.noreply.github.com>
2023-05-14 23:17:31 +0530

    added hosting funtionality with ngrok

--- Meme_Website_Flask/main.py
+++ Meme_Website_Flask/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from flask_ngrok import run_with_ngrok
 import requests
 import json
 
 app = Flask(__name__, template_folder=r'C:\Users\shrey\OneDrive\Documents\My-Projects\Meme_Website_Flask\templates')
+run_with_ngrok(app)
 
 def get_meme():
     url = "https://meme-api.com/gimme"
