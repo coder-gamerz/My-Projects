ed850da6dc678145354686223451c1cfede09d32
coder-gamerz <shrey1559@gmail.com>
2023-05-19 17:04:55 +0530

    modified the app files to make it look better

--- Todo-App/Todo-App/main.py
+++ Todo-App/Todo-App/main.py
@@ -1,99 +1,113 @@
-from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
-from database import Database
+import customtkinter as ctk
+import getpass
+from packages import *
 
-db = Database('store.db')
-def populate_todo_list():
-    todo_list.delete(0, END)
-    for item in db.fetch():
-        todo_list.insert(END, [item[0], item[1], item[2], item[3]])
-    return todo_list.get(0, END)
+USER_NAME = getpass.getuser()
 
-def add_todo_item(*args):
-    if todo_text.get() == '':
-        messagebox.showerror('Require Input', 'Todo entry is required')
-        return
+AT_THE_BEGINNING = True
 
-    db.insert(todo_text.get())
-    todo_list.insert(END, (todo_text.get()))
-    populate_todo_list()
-    clear_box()
-    todo_list.see(END)
+STORAGE = rf"C:\Users\shrey\OneDrive\Documents\My-Projects\Todo-App\Todo-App\tasks.txt"
 
-def delete_todo_item():
+
+def save_todo():
+    file = open(STORAGE, "w")
+
+    for frame in scrollable_frame.winfo_children():
+        for widget in frame.winfo_children():
+            if isinstance(widget, ctk.CTkLabel):
+                file = open(STORAGE, "a")
+                file.write(f"{widget.cget('text')}\n")
+
+    file.close()
+
+
+def delete_todo(item, content):
+    item.destroy()
+
+    save_todo()
+
+
+def add_todo(_text=""):
+    def add(t):
+        frame = ctk.CTkFrame(scrollable_frame)
+
+        label = ctk.CTkLabel(frame, text=t, font=ctk.CTkFont(size=18), width=670)
+
+        delete_check = ctk.CTkCheckBox(
+            frame, text="", command=lambda: delete_todo(frame, label)
+        )
+
+        frame.pack()
+        label.grid(row=0, column=1)
+        delete_check.grid(row=0, column=0)
+
+        entry.delete(0, ctk.END)
+
+        save_todo()
+
+    todo = entry.get()
+
+    if todo != "":
+        add(todo)
+    else:
+        if _text != "":
+            add(_text)
+
+
+def check_keypress(event):
+    if event.keysym == "Return":
+        add_todo()
+
+
+window = ctk.CTk()
+window.geometry("750x450")
+window.title("ToDo App")
+
+
+title_label = ctk.CTkLabel(
+    window, text="Daily Tasks", font=ctk.CTkFont(size=20, weight="bold")
+)
+title_label.grid(row=0, column=0, padx=10, pady=10)
+
+
+scrollable_frame = ctk.CTkScrollableFrame(window, width=700, height=370)
+scrollable_frame.grid(row=1, column=0, padx=10, pady=(0, 20))
+
+
+entry_container = ctk.CTkFrame(scrollable_frame, width=700)
+entry_container.pack()
+
+
+entry = ctk.CTkEntry(entry_container, placeholder_text="Add item", width=670)
+entry.grid(row=0, column=0, padx=(0, 5))
+
+
+add_button = ctk.CTkButton(
+    entry_container,
+    text="+ ",
+    font=ctk.CTkFont(size=18, weight="bold"),
+    width=30,
+    command=add_todo,
+)
+add_button.grid(row=0, column=1)
+
+
+window.bind("<KeyPress>", check_keypress)
+
+
+if AT_THE_BEGINNING:
     try:
-        index_item = todo_list.curselection()[0]
-        selected_item = todo_list.get(index_item)
-        db.delete(selected_item[0])
-        populate_todo_list()
-        clear_box()
-    except IndexError:
-        messagebox.showerror('Delete error', 'Select item for deleting')
+        file = open(STORAGE, "r")
+        lines = file.read().splitlines()
+        file.close()
 
-def update_todo_item():
-    try:
-        index_item = todo_list.curselection()[0]
-        selected_item = todo_list.get(index_item)
-        db.update(selected_item[0], todo_text.get())
-        populate_todo_list()
-        clear_box()
-    except IndexError:
-        messagebox.showerror('Update error', 'Select item for updating')
+        for line in lines:
+            add_todo(line)
 
-def select_item(*args):
-    index_item = todo_list.curselection()[0]
-    selected_item = todo_list.get(index_item)
-    todo_text.set(selected_item[1])
-    created_at_var.set(selected_item[2].strftime("%b %d %Y"))
-    updated_at_var.set(selected_item[3].strftime("%b %d %Y"))
-    print(index_item)
+    except FileNotFoundError:
+        print(f"Failed to open {STORAGE}")
 
-def clear_box():
-    todo_entry.delete(0, END)
+    AT_THE_BEGINNING = False
 
-app = Tk()
-style = ttk.Style()
-mainframe = ttk.Frame(app, padding=(3,3,12,12))
-mainframe.grid(column=0, row=0)
-todo_title = Label(mainframe, text='Todo App', font=('bold', 16))
-todo_title.grid(column=0, row=0, sticky=(W, E), columnspan=3, pady=10)
-todo_text = StringVar()
-todo_lbl = ttk.Label(mainframe, text='Your Entry')
-todo_lbl.grid(column=0, row=1, sticky=W)
-todo_entry = ttk.Entry(mainframe, textvariable=todo_text, width=50)
-todo_entry.grid(column=1, row=1, ipadx=5, ipady=5, padx=5, pady=5, sticky=(W, E))
-add_btn = ttk.Button(mainframe, text='Add', command=add_todo_item)
-add_btn.grid(column=2, row=1, sticky=E, ipadx=4, ipady=4)
-created_at_lbl = ttk.Label(mainframe, text='Created at:')
-created_at_lbl.grid(column=1, row=2, sticky=W, ipady=5, ipadx=5, padx=5, pady=5)
-created_at_lbl = ttk.Label(mainframe, text='Updated at:')
-created_at_lbl.grid(column=1, row=2, sticky=E, ipady=5, ipadx=5, padx=68, pady=5)
-created_at_var = StringVar()
-created_at_date = ttk.Label(mainframe, textvariable=created_at_var)
-created_at_date.grid(column=1, row=2, sticky=W, padx=68)
-updated_at_var = StringVar()
-updated_at_date = ttk.Label(mainframe, textvariable=updated_at_var)
-updated_at_date.grid(column=1, row=2, sticky=E, padx=5)
-list_text = StringVar()
-todo_list = Listbox(mainframe, width=70, height=10, border=0)
-todo_list.grid(column=1, row=3, sticky=(W,E), ipady=5, ipadx=5, padx=5, pady=5)
-del_btn = ttk.Button(mainframe, text='Delete Item', command=delete_todo_item)
-del_btn.grid(column=1, row=4, sticky=W, ipady=5, ipadx=5, padx=5, pady=5)
-clr_btn = ttk.Button(mainframe, text='Clear Input', command=clear_box)
-clr_btn.grid(column=1, row=4, sticky=NS, ipady=5, ipadx=5, padx=5, pady=5)
-update_btn = ttk.Button(mainframe, text='Update Item', command=update_todo_item)
-update_btn.grid(column=1, row=4, sticky=E, ipady=5, ipadx=5, padx=5, pady=5)
-scrlbr = ttk.Scrollbar(mainframe, orient=VERTICAL, command=todo_list.yview)
-scrlbr.grid(column=2, row=3, sticky=W)
-todo_list.configure(yscrollcommand=scrlbr.set, height=12)
-todo_list.bind('<<ListboxSelect>>', select_item)
-app.bind('<Return>', add_todo_item)
-populate_todo_list()
 
-for item in range(0, len(populate_todo_list()), 2):
-    todo_list.itemconfigure(item, background='#ccc')
-    
-app.title("Todo App")
-app.geometry('600x400')
-app.mainloop()
+window.mainloop()
---
+++ Todo-App/Todo-App/packages.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+from customtkinter import *
+from tkinter import filedialog
+from tkinter import font
+import distutils
+from distutils import *
+import darkdetect
+from darkdetect import *
+import ctypes.util
+from tkinter import ttk
