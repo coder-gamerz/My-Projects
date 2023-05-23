fffba573282b61636ccf79db6cebccb2d9d7a014
coder-gamerz <shrey1559@gmail.com>
2023-05-23 14:27:41 +0530

    added better functionality

--- Beginner Projects/paint.py
+++ Beginner Projects/paint.py
@@ -6,7 +6,7 @@ import pyscreenshot as ImageGrab
 from PIL import ImageTk,Image
 
 
-class Color_Button(Button):
+class Color_Button(Button):#Inherited custom made color button , with default command to change colour of pen
     def __init__(self,color):
         self.color=color
         super().__init__(bg=color,relief=RIDGE,width=2,height=1,bd=2,command=self.on_click)
@@ -20,32 +20,32 @@ class main:
         screen_width = root.winfo_screenwidth()
         screen_height = root.winfo_screenheight()
         self.root.geometry(f"{screen_width-int(screen_width*0.3)}x{screen_height-int(screen_height*0.3)}")
-        self.color_fg = 'black'  
-        self.color_bg = 'white'  
+        self.color_fg = 'black'  # Colour of pen
+        self.color_bg = 'white'  # Background colour
         self.root.configure(background='white')
 
         self.undo = open('undo.rec', 'w')
 
 
-        
-        colors = ['
-        i=5;j=5
+        # making colour buttons
+        colors = ['#F8F8FF', '#cc1701','#0bd3d3','#a845b5', '#f1c232' , '#FF00FF']
+        i=5;j=5#For Location
         for color in colors:
-            Color_Button(color).place(x=i,y=j)
+            Color_Button(color).place(x=i,y=j)#Refer class Color_Button
             j+=30
-        colors = [ 'black', '
+        colors = [ 'black', '#ffa500','red' ,'#009888', 'blue','#2bdf94']
         i=35;j=5
         for color in colors:
             Color_Button(color).place(x=i,y=j)
             j+=30
 
-        
+        # making other buttons and placing them in appropriate place
         self.Pallet_For_Foreground = Button(self.root, text='PALLET', font=('calibri', 10), bd=3, bg='white', command=self.choose_color,width=8, relief=RIDGE)
         self.Pallet_For_Foreground.place(x=0,y=185)
 
 
-        self.erazer_button = Button(self.root, text='ERAZE', font=('calibri', 10), bd=3, bg='white', command=self.eraze,width=8, relief=RIDGE)
-        self.erazer_button.place(x=0, y=215)
+        self.ERASEr_button = Button(self.root, text='ERASE', font=('calibri', 10), bd=3, bg='white', command=self.ERASE,width=8, relief=RIDGE)
+        self.ERASEr_button.place(x=0, y=215)
 
         self.clear_button = Button(self.root, text='CLEAR', bd=3, bg='white', command=self.clear, width=8, relief=RIDGE)
         self.clear_button.place(x=0, y=245)
@@ -59,7 +59,7 @@ class main:
 
 
         self.options = StringVar(self.root)
-        self.options.set('3')
+        self.options.set('3')#Default option
         self.size_chooser = OptionMenu(self.root, self.options, *( str(x) for x in range(10)))
         self.size_chooser.place(x=0, y=330)
 
@@ -80,7 +80,7 @@ class main:
         self.Background_Button = Button(self.root, text='BG COLOR', font=('calibri', 10),bd=3, bg='white', command=self.change_bg, width=8, relief=RIDGE)
         self.Background_Button.place(x=0, y=490)
 
-        
+        # making the canvas and making it recognise movements of the mouse
 
         self.canvas = Canvas(self.root, bg='white', bd=3, relief=GROOVE, height=(screen_height-int(screen_height*0.33)), width=(screen_width-int(screen_width*0.37)))
         self.canvas.place(x=75, y=3)
@@ -89,11 +89,11 @@ class main:
         self.old_y = None
         self.penwidth = 3
         self.tag = 0
-        self.canvas.bind('<B1-Motion>', self.paint)  
+        self.canvas.bind('<B1-Motion>', self.paint)  # drawing  line
         self.canvas.bind('<ButtonRelease-1>', self.reset)
 
 
-    
+    # All functions are defined below
     def paint(self, e):
         '''creates a canvas where you can paint'''
 
@@ -130,7 +130,7 @@ class main:
         '''clears the canvas'''
         self.canvas.delete(ALL)
 
-    def eraze(self):
+    def ERASE(self):
         '''erazing stuff simply by changing colour of the brush to white'''
         self.undo.write(f"self.color_fg='{self.color_fg}'\n")
         self.undo.write(f"self.options.set({self.options.get()})\n")
@@ -139,7 +139,7 @@ class main:
 
     def undo_exec(self):
         self.undo.close()
-        
+        # Find last line and execute it
         file = open("copy.txt", 'w')
         with open("undo.rec", 'r') as undo:
             length = sum(1 for x in undo)
@@ -161,14 +161,14 @@ class main:
         self.undo = open("undo.rec", 'a')
 
     def save(self):
-        
+        # Asking format
         filename: str = asksaveasfilename(initialdir="/Desktop", title="Select file", filetypes=(
             ('JPEG', '*.jpg'), ('PNG', '*.png'), ('PS', '*.ps'),
             ('BMP', '*.bmp'),
             ('GIF', '*.gif')),defaultextension=".jpg")
         print(filename)
         if filename.endswith('.ps'):
-            
+            # Saving a postscript file
             try:
                 self.canvas.postscript(file=filename, colormode='color')
                 messagebox.showinfo('File saved : ', str(filename))
@@ -177,7 +177,7 @@ class main:
 
         else:
             try:
-                
+                # Making values for a screenshort
                 x = self.root.winfo_rootx() + self.canvas.winfo_x()
                 y = self.root.winfo_rooty() + self.canvas.winfo_y()
                 x1 = x + self.canvas.winfo_width()
@@ -200,10 +200,10 @@ class main:
             return()
 
     def choose_color(self):
-        
+        # variable to store hexadecimal code of color
         self.undo.write(f"self.color_fg='{self.color_fg}'\n")
         color_code = colorchooser.askcolor(title="Choose color")
-        self.color_fg=color_code[1]
+        self.color_fg=color_code[1]#colour_code[1] is hex value of chose color. color_code[0] is rgb value of chosen color
 
     def change_bg(self):
         self.undo.write(f"self.canvas['bg']='{self.color_bg}'\n")
@@ -232,7 +232,7 @@ class main:
             messagebox.showwarning("That isn't working")
 
 if __name__ == '__main__':
-    
+    #For Activating
     root = Tk()
     program=main(root)
     root.title('Paint')
