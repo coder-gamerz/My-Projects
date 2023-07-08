0658ac09a499968c67b5afcad3667a0c2ac27035
coder-gamerz <shrey1559@gmail.com>
2023-07-08 14:58:29 +0530

    fixed errors

--- Games/Pacman/main.py
+++ Games/Pacman/main.py
@@ -16,13 +16,13 @@ color = 'blue'
 PI = math.pi
 player_images = []
 for i in range(1, 5):
-    player_images.append(pygame.transform.scale(pygame.image.load(fr'C:\Users\shrey\OneDrive\Documents\My-Projects\Games\Pacman\assets/player_images/{i}.png'), (45, 45)))
-blinky_img = pygame.transform.scale(pygame.image.load(fr'C:\Users\shrey\OneDrive\Documents\My-Projects\Games\Pacman\assets/ghost_images/red.png'), (45, 45))
-pinky_img = pygame.transform.scale(pygame.image.load(fr'C:\Users\shrey\OneDrive\Documents\My-Projects\Games\Pacman\assets/ghost_images/pink.png'), (45, 45))
-inky_img = pygame.transform.scale(pygame.image.load(fr'C:\Users\shrey\OneDrive\Documents\My-Projects\Games\Pacman\assets/ghost_images/blue.png'), (45, 45))
-clyde_img = pygame.transform.scale(pygame.image.load(fr'C:\Users\shrey\OneDrive\Documents\My-Projects\Games\Pacman\assets/ghost_images/orange.png'), (45, 45))
-spooked_img = pygame.transform.scale(pygame.image.load(fr'C:\Users\shrey\OneDrive\Documents\My-Projects\Games\Pacman\assets/ghost_images/powerup.png'), (45, 45))
-dead_img = pygame.transform.scale(pygame.image.load(fr'C:\Users\shrey\OneDrive\Documents\My-Projects\Games\Pacman\assets/ghost_images/dead.png'), (45, 45))
+    player_images.append(pygame.transform.scale(pygame.image.load(fr'/home/codergamerz/Documents/My-Projects/Games/Pacman/assets/player_images/{i}.png'), (45, 45)))
+blinky_img = pygame.transform.scale(pygame.image.load(fr'/home/codergamerz/Documents/My-Projects/Games/Pacman/assets/ghost_images/red.png'), (45, 45))
+pinky_img = pygame.transform.scale(pygame.image.load(fr'/home/codergamerz/Documents/My-Projects/Games/Pacman/assets/ghost_images/pink.png'), (45, 45))
+inky_img = pygame.transform.scale(pygame.image.load(fr'/home/codergamerz/Documents/My-Projects/Games/Pacman/assets/ghost_images/blue.png'), (45, 45))
+clyde_img = pygame.transform.scale(pygame.image.load(fr'/home/codergamerz/Documents/My-Projects/Games/Pacman/assets/ghost_images/orange.png'), (45, 45))
+spooked_img = pygame.transform.scale(pygame.image.load(fr'/home/codergamerz/Documents/My-Projects/Games/Pacman/assets/ghost_images/powerup.png'), (45, 45))
+dead_img = pygame.transform.scale(pygame.image.load(fr'/home/codergamerz/Documents/My-Projects/Games/Pacman/assets/ghost_images/dead.png'), (45, 45))
 player_x = 450
 player_y = 663
 direction = 0
