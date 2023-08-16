44188d24fead04a717a65094ab7d55d61116f71a
coder-gamerz <shrey1559@gmail.com>
2023-08-16 19:21:39 +0530

    edit

--- Meme_Website_Flask/main.py
+++ Meme_Website_Flask/main.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template
 import requests
 import json
 
-app = Flask(__name__, template_folder=r'C:\Users\shrey\OneDrive\Documents\My-Projects\Meme_Website_Flask\templates')
+app = Flask(__name__, template_folder=r'/home/codergamerz/Documents/My-Projects/Meme_Website_Flask/templates/')
 
 def get_meme():
     url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
-    return meme_large
+
 
 @app.route('/')
 def index():
     meme_pic = get_meme()
     return render_template("index.html", meme_pic=meme_pic)
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=5001, debug=True)
